6da56007281321348e72cdf6cb70d7b3acdd6099
Joel Ross <joelross@uw.edu>
2017-04-10 22:16:48 -0700

    Add back button support

--- app/src/main/java/edu/uw/fragmentdemo/MainActivity.java
+++ app/src/main/java/edu/uw/fragmentdemo/MainActivity.java
@@ -78,4 +78,16 @@ public class MainActivity extends AppCompatActivity implements MoviesFragment.On
             }
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }
 }
